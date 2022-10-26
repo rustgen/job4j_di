75322968dd72c1a765f9f6f7a5d6e221afde6cba
new_user <nrustgen@gmail.com>
2022-10-26 02:11:40 -0700

    4. Lifetime (Scope) [#261277]

--- src/main/java/ru/job4j/di/SpringDI.java
+++ src/main/java/ru/job4j/di/SpringDI.java
@@ -7,11 +7,24 @@ public class SpringDI {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
         context.scan("ru.job4j.di");
         context.refresh();
+
+        Store store = context.getBean(Store.class);
+        store.add("John");
+        store.add("Mark");
+        Store storeAnother = context.getBean(Store.class);
+        storeAnother.add("Wesley");
+        storeAnother.getAll().forEach(System.out::println);
+
         StartUI ui = context.getBean(StartUI.class);
-        String name = ui.getAskStr("Put your name before question.");
+        String name = ui.getAskStr("Put your name");
         ui.add(name);
-        ui.add("Petr Arsentev");
-        ui.add("Ivan ivanov");
+        ui.add("Zena");
+        ui.add("Ivan");
         ui.print();
+        StartUI uiAnother = context.getBean(StartUI.class);
+        String nameAnother = uiAnother.getAskStr("Put your name");
+        uiAnother.add(name);
+        uiAnother.add("Ashley");
+        uiAnother.print();
     }
 }
